baeaa072c9323bb3238b64fd29529af438e57772
DiegoQuiroz420 <al02994936@tecmilenio.mx>
2023-10-07 15:55:26 -0600

    inicio/prueba

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[20];
 
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Ingrese 20 números enteros:");
 
-            System.out.println("i = " + i);
+        for (int i = 0; i < 20; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        System.out.println("\nNúmero | Cuadrado | Cubo");
+        System.out.println("------------------------");
+
+        for (int i = 0; i < 20; i++) {
+            int numero = numeros[i];
+            int cuadrado = numero * numero;
+            int cubo = numero * numero * numero;
+
+            System.out.println(numero + "      | " + cuadrado + "       | " + cubo);
         }
     }
 }
